9e015801dbd558a9351cad006307ac6deed43d33
Nguyen Nguyen Nhat <nguyennhatnguyen1995@gmail.com>
2024-10-04 11:06:40 +0700

    Update code

--- src/screens/ListProductScreen.tsx
+++ src/screens/ListProductScreen.tsx
@@ -1,7 +1,6 @@
-import React, {useCallback, useRef} from 'react';
+import React, {useCallback} from 'react';
 import {SafeAreaView, StyleSheet} from 'react-native';
 import ProductTemplate from '../components/templates/ProductTemplate';
-// import NaverMapView from '@mj-studio/react-native-naver-map';
 import {useAppDispatch, useAppSelector} from '../redux/store';
 import {
   addProduct,
@@ -11,7 +10,6 @@ import {
 import {Product} from '../types/product';
 
 const ListProductScreen: React.FC = () => {
-  // const ref = useRef(null);
   const products = useAppSelector(productSlice.selectors.selectProducts);
   const dispatch = useAppDispatch();
 
@@ -31,7 +29,6 @@ const ListProductScreen: React.FC = () => {
 
   return (
     <SafeAreaView style={styles.container}>
-      {/* <NaverMapView ref={ref} style={{flex: 1}} /> */}
       <ProductTemplate
         products={products}
         onDelete={_handleDelete}
